827d7b3e4c71a3824e7502556a1e04a0396ba9f2
Francesco Tassi <tassi.francesco@gmail.com>
2024-04-16 09:22:43 +0200

    Major update
    Extract various roles.
    Refactor playbook:
    * base.yml take care of basic deps and ssh identity
    * distrobox-dev.yml is my base dev setup. This requires a little more
      tweaking yet
    * distrobox-nvim.yml is my nvim ide like setup with all lsp related deps
      included. It seems to be workgin fine at the time of writing, even
      though I have not used it much yet.
    * gui.yml install flatpak and a bunch of gui app. Non really sure if I
      want to distrobox them as well or not. Ideally I would like to have
      "preconfigured" gui app (eg firefox with all my add on installed and
      configured).

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-apt-get install software-properties-common
-apt-add-repository ppa:ansible/ansible
-apt-get update
-apt-get install ansible git
+apt-add-repository -y ppa:ansible/ansible
+apt update -y
+apt-get install -y software-properties-common
+apt-get install -y ansible git podman 
