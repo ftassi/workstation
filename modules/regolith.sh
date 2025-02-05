346c40fc787602ca3c3a91372ba53d287472dfa5
Francesco Tassi <tassi.francesco@gmail.com>
2025-02-05 01:13:04 +0100

    Regolith module

---
+++ modules/regolith.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Inclusione delle funzioni comuni
+source "$(dirname "$0")/../common.sh"
+
+info "Installazione di regolith-desktop..."
+
+info "Aggiunta della chiave GPG di Regolith..."
+wget -qO - https://regolith-desktop.org/regolith.key | \
+gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
+
+info "Aggiunta del repository di Regolith..."
+echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
+https://regolith-desktop.org/release-3_2-ubuntu-jammy-amd64 jammy main" | \
+sudo tee /etc/apt/sources.list.d/regolith.list
+
+info "Aggiornamento dei repository e installazione di regolith-desktop..."
+sudo apt update
+sudo apt install regolith-desktop regolith-session-flashback regolith-look-lascaille
+
+success "Regolith Desktop installato con successo!"
+success "Riavvia il sistema per attivare Regolith Desktop."
