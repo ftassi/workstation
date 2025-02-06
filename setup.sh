29cdc2d4a2b65afd755ff1e65d2b009b1dd85381
Francesco Tassi <tassi.francesco@gmail.com>
2025-02-06 09:51:11 +0100

    Remove the creation of a .password file
    A .password file is not longer required since all the secrets are
    handled by git-crypt. The only time a mnemonic master password is
    required is when decrypting the git-crypt key.

--- setup.sh
+++ setup.sh
@@ -8,24 +8,6 @@ source "$(dirname "$0")/common.sh"
 # Ottieni il nome dell'utente corrente (quello che vuoi abilitare)
 CURRENT_USER=$(logname 2>/dev/null || echo "$SUDO_USER")
 
-# Se il file delle password esiste, informiamo l'utente
-if [ -f "$PASSWORD_FILE" ]; then
-    info "Il file .passwords esiste già. Skipping."
-else
-    info "Creazione del file .passwords..."
-
-    info "[INPUT] Inserisci la master password:"
-    read -s MASTER_PASSWORD
-    echo ""
-
-    sudo -u "$SUDO_USER" echo "MASTER_PASSWORD=$MASTER_PASSWORD" > "$PASSWORD_FILE"
-
-    chmod 600 "$PASSWORD_FILE"
-    
-    success "File .passwords creato con successo."
-fi
-
-
 info "Aggiunta dell'utente $CURRENT_USER ai sudoers..."
 info "Dopo il provisioning elimina il file /etc/sudoers.d/zz_provisioning_${CURRENT_USER} per rimuovere i privilegi."
 
@@ -58,8 +40,9 @@ if [ ! -f "git-crypt.key.gpg" ]; then
     exit 1
 fi
 
-# Recupera la master password dal file .passwords
-MASTER_PASSWORD=$(get_master_password)
+info "[INPUT] Inserisci la master password:"
+read -s MASTER_PASSWORD
+echo ""
 
 # Decripta la chiave AES di git-crypt
 gpg --batch --yes --passphrase "$MASTER_PASSWORD" --output git-crypt.key --decrypt git-crypt.key.gpg
